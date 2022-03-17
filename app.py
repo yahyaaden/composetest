9517051c27ec741473532986821ddd0278a27738
unknown <yahya.a.aden@gmail.com>
2022-03-17 10:10:18 +0100

    Update

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker my friends! I have been seen {} times. Thank you very much and please come again!\n'.format(count)
+    return 'Hey! This page has been {} times.\n'.format(count)
